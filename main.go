65a06ebf070b48b5bb4bade57ecd13f187a32ff5
Jeremy Olliver <jeremy.olliver@loyalty.co.nz>
2019-12-09 22:28:51 +1300

    WIP sample CLI skeleton

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/devfacet/gocmd"
+	"math"
+	"strings"
+)
+
+func main() {
+	flags := struct {
+		Help    bool `short:"h" long:"help" description:"Display usage" global:"true"`
+		Version bool `short:"v" long:"version" description:"Display version"`
+		Echo    struct {
+			Settings bool `settings:"true" allow-unknown-arg:"true"`
+		} `command:"echo" description:"Print arguments"`
+		Math struct {
+			Sqrt struct {
+				Number float64 `short:"n" long:"number" required:"true" description:"Number"`
+			} `command:"sqrt" description:"Calculate square root"`
+			Pow struct {
+				Base     float64 `short:"b" long:"base" required:"true" description:"Base"`
+				Exponent float64 `short:"e" long:"exponent" required:"true" description:"Exponent"`
+			} `command:"pow" description:"Calculate base exponential"`
+		} `command:"math" description:"Math functions" nonempty:"true"`
+	}{}
+
+	// Echo command
+	gocmd.HandleFlag("Echo", func(cmd *gocmd.Cmd, args []string) error {
+		fmt.Printf("%s\n", strings.Join(cmd.FlagArgs("Echo")[1:], " "))
+		return nil
+	})
+
+	// Math commands
+	gocmd.HandleFlag("Math.Sqrt", func(cmd *gocmd.Cmd, args []string) error {
+		fmt.Println(math.Sqrt(flags.Math.Sqrt.Number))
+		return nil
+	})
+	gocmd.HandleFlag("Math.Pow", func(cmd *gocmd.Cmd, args []string) error {
+		fmt.Println(math.Pow(flags.Math.Pow.Base, flags.Math.Pow.Exponent))
+		return nil
+	})
+
+	// Init the app
+	gocmd.New(gocmd.Options{
+		Name:        "sonic",
+		Version:     "1.0.0",
+		Description: "Find and connect wherever you need to go",
+		Flags:       &flags,
+		ConfigType:  gocmd.ConfigTypeAuto,
+	})
+}
